0a83aeca3ce7196ded053dbe4378daa4a0b1050f
Ivo Bathke <ivo.bathke@gmail.com>
2020-05-07 17:45:47 +0200

    Variant title fallback

--- src/Resolver/TitleWithVariant.php
+++ src/Resolver/TitleWithVariant.php
@@ -2,8 +2,18 @@
 
 namespace IvobaOxid\Exporter\Resolver;
 
+use OxidEsales\Eshop\Core\Database\Adapter\DatabaseInterface;
+
 class TitleWithVariant extends BaseResolver
 {
+    private $db;
+
+    public function __construct(DatabaseInterface $db, string $supports)
+    {
+        $this->db = $db;
+        parent::__construct($supports);
+    }
+
     /**
      * @param array $data
      * @return string
@@ -11,6 +21,13 @@ class TitleWithVariant extends BaseResolver
     public function resolve(array $data): string
     {
         $title = $data['OXTITLE'];
+        if (!$title && $data['OXPARENTID']) {
+            $sql    = 'select oxtitle from oxarticles where oxid = "'.$data['OXPARENTID'].'" limit 1';
+            $result = $this->db->select($sql);
+            if ($result !== false && $result->count() > 0) {
+                $title = $result->fields['oxtitle'];
+            }
+        }
         if ($data['OXVARSELECT'] !== '') {
             $title .= ' '.$data['OXVARSELECT'];
         }
